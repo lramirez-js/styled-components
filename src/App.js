285ef01fe5bf1721fda80166e469c241abfb202b
lramirez-js <luis.rg@outlook.com>
2022-09-22 02:47:51 -0500

    First styled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const P = styled.p`
+    font-size: 24px;
+    color: red;
+  `
+
+  return(
+    <div>
+      <P>Hi there!</P>
     </div>
-  );
+  )
 }
 
 export default App;
